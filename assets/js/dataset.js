ccfbd9d4596a110c1f2c53e1b88825f78d91263f
ruizhe <eraser567@163.com>
2017-07-07 11:32:32 +0800

    start to modify the bootstrap table to be like kms

---
+++ assets/js/dataset.js
@@ -0,0 +1,29 @@
+function load(db) {
+	//alert("to load"+db);
+	// 应该先unload，再load
+	$.get("load/"+db, function(data, status){
+		//alert("in get");
+		if(status=="success"){
+			alert(data);
+		}
+	});
+}
+
+function query(dp) {
+	$.get("query/" + dp, function(data, status){
+		if(status=="success"){
+			$("#queryAns").empty();
+			var res = $("<p></p>").text(data);
+			$("#queryAns").append(res);
+			$("#queryAns").scrollTop($("#queryAns").height());
+		}
+	});
+}
+
+function unload(db) {
+	$.get("unload", function(data, status){
+		if(status=="success"){
+			alert(data);
+		}
+	});
+}
