051bba9080fc74fd2af2272ad8ef115751058076
ruizhe <eraser567@163.com>
2017-07-09 00:43:11 +0800

    pagination done, search half done

--- assets/js/gstore-table.js
+++ assets/js/gstore-table.js
@@ -21,8 +21,8 @@ function doInsertRow(tb, title, descritpion) {
     var dataId = randomString(5);    
     var rowHtml = `<td>${title}</td> <td>${descritpion}</td> <td align=\"center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td> <td align=\"center\"><button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-trash\"></span></button></td>`;
 
-    console.log(title);
-    console.log(descritpion);
+    // console.log(title);
+    // console.log(descritpion);
 
     var tr = document.createElement('tr');
     tr.innerHTML = rowHtml;
