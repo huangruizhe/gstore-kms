87c6a1c84822ccfaf61e08cce93cee5447de94f2
ruizhe <eraser567@163.com>
2017-07-10 03:33:23 +0800

    load data from gstore done

--- assets/js/gstore-table.js
+++ assets/js/gstore-table.js
@@ -1,9 +1,24 @@
-function doInsertRow(tb, title, descritpion) {
-    var dataId = randomString(5);    
-    var rowHtml = `<td>${title}</td> <td>${descritpion}</td> <td align=\"center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td> <td align=\"center\"><button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-trash\"></span></button></td>`;
+function getRowHtml(title, description, uri) {
+    var dataId = randomString(5);
+
+    var maxlen = 80;
+
+    var rawHtml;
+    if (description.length > maxlen) {
+        var shortDescription = description.substring(0, maxlen) + " ...";
+        rowHtml = `<tr> <td data-uri='${uri}'>${title}</td> <td title='${description}'>${shortDescription}</td> <td align=\"center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td> <td align=\"center\"><button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-trash\"></span></button></td> </tr>`;
+    } else {
+        rowHtml = `<tr> <td data-uri='${uri}'>${title}</td> <td>${description}</td> <td align=\"center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td> <td align=\"center\"><button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" data-id=\"${dataId}\"><span class=\"glyphicon glyphicon-trash\"></span></button></td> </tr>`;        
+    }
+    return rowHtml;
+}
+
+function doInsertRow(tb, title, description) {
+    var uri = "http://www.founder/" + randomString(5);
+    var rowHtml = getRowHtml(title, description, uri);
 
     // console.log(title);
-    // console.log(descritpion);
+    // console.log(description);
 
     var tr = document.createElement('tr');
     tr.innerHTML = rowHtml;
@@ -15,11 +30,35 @@ function doDelRow(tr) {
     tr.fadeOut('slow', () => tr.remove());
 }
 
-function doUpdateRow(tr, title, descritpion) {
+function doUpdateRow(tr, title, description) {
     tr.find('td:first-child').text(title);
-    tr.find('td:nth-child(2)').text(descritpion);
+    tr.find('td:nth-child(2)').text(description);
 }
 
 var buttonId = undefined;
 
+function loadAllData(n, on_success, on_fail) {
+    var sparql = "select ?subject ?name ?intro where {?subject <http://www.founder.attr:name> ?name. ?subject <http://www.founder.attr:简介> ?intro. } limit " + n;
+
+    var url = `http://${global_ip}:${global_port}/query/\"${sparql}\"`;
+    $.get(
+        encodeURI(url),
+        on_success
+    ).fail(function () {
+        on_fail();
+    });
+}
+
+function parseAllData (data) {
+    var obj = JSON.parse(data.replace(/"value": "\s+/g,'"value": "').replace(/\s+" }/g, '" }'));
+    var result = [];
+
+    // var vars = obj["head"]["vars"];
+    var bindings = obj["results"]["bindings"];
+    for (var i in bindings) {
+        result.push([bindings[i]["subject"]["value"], bindings[i]["name"]["value"], bindings[i]["intro"]["value"]]);
+    }
+
+    return result;
+}
 
--- assets/js/gstore-util.js
+++ assets/js/gstore-util.js
@@ -7,6 +7,19 @@ String.prototype.trim = function () {
     return this.replace(/(^\s*)|(\s*$)/g, '');
 };
 
+// 判断字符串长度并且区分中文和英文
+String.prototype.gblen = function() {  
+  var len = 0;  
+  for (var i=0; i<this.length; i++) {  
+    if (this.charCodeAt(i)>127 || this.charCodeAt(i)==94) {  
+       len += 2;  
+     } else {  
+       len ++;  
+     }  
+   }  
+  return len;  
+}
+
 // 生成随机字符串序列
 function randomString (len) {
     　　len = len || 32;
